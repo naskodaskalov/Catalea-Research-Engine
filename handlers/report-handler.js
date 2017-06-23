74a9d2225ae3c600f0b0e6deb22965804e4bb996
Nasko Daskalov <nasko.daskalov@abv.bg>
2017-06-24 01:23:01 +0300

    Validation if there are any reports for choosen company

--- handlers/report-handler.js
+++ handlers/report-handler.js
@@ -15,7 +15,6 @@ module.exports = (req, res) => {
 
     if (req.method === 'POST') {
       let financialFiles = ''
-      let fileName = 'tuk'
       let parsedFeed = ''
       let companyInfo = {
         'companyName': '',
@@ -30,7 +29,6 @@ module.exports = (req, res) => {
         let name = parser.parse(result)
         currentCompanyName = name.companyName
         companyInfo.companyName = currentCompanyName
-        console.log(companyInfo.companyName)
       })
 
       fs.readFile('./content/cik-lookup-data.txt', 'utf-8', (err, file) => {
@@ -44,7 +42,6 @@ module.exports = (req, res) => {
         let currentCompanyNameLenght = (Number)(currentCompanyName.length)
         let currentCompanyCIKNumberIndex = (Number)(currentCompanyNameIndex + currentCompanyNameLenght + 1)
         currentCompanyCIK = readedFile.slice(currentCompanyCIKNumberIndex, (currentCompanyCIKNumberIndex) + 10)
-        console.log(currentCompanyCIK)
 
         let newUrl = `https://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&CIK=${currentCompanyCIK}&CIK=${currentCompanyCIK}&type=10-K%25&dateb=20161231&owner=include&start=0&count=10&output=atom`
 
@@ -54,29 +51,28 @@ module.exports = (req, res) => {
             return
           }
           parsedFeed = parsed.feed.entries
-          for (let feed of parsedFeed) {
-            let reportType = feed.title.slice(0, 4)
-            let reportPulishDate = new Date(feed.pubDate)
-            let reportTxtLink = feed.link.slice(0, -10) + '.txt'
-            console.log(reportTxtLink)
-            // console.log(feed.title)
-            // console.log(feed.pubDate)
-            // console.log(feed.link)
+
+          if (parsedFeed.length === 0) {
             financialFiles += `
                               <tr>
-                                <td class="text-left">${reportType}</td>
-                                <td class="text-left">${reportPulishDate.toUTCString().slice(4, 16)}</td>
-                                <td class="text-left"><a href="${reportTxtLink}">${feed.title}</a></td>
+                                <td class="text-left" colspan="3">No Information!</td>
                               </tr>`
+          } else {
+            for (let feed of parsedFeed) {
+              let reportType = feed.title.slice(0, 4)
+              let reportPulishDate = new Date(feed.pubDate)
+              let reportTxtLink = feed.link.slice(0, -10) + '.txt'
+              financialFiles += `
+                                <tr>
+                                  <td class="text-left">${reportType}</td>
+                                  <td class="text-left">${reportPulishDate.toUTCString().slice(4, 16)}</td>
+                                  <td class="text-left"><a href="${reportTxtLink}">${feed.title}</a></td>
+                                </tr>`
+            }
           }
-          console.log('parsing finish')
-          // console.log(parsedFeed[0].pubDate)
           companyInfo.companyCik = currentCompanyCIK
-
-          console.log('rendering company info')
           data = data.replace('{{companyName}}', companyInfo.companyName)
           data = data.replace('{{companyCik}}', companyInfo.companyCik)
-          console.log('rendering financial info')
           data = data.replace('{{financialFiles}}', financialFiles)
           res.writeHead(200, {
             'Content-Type': 'text/html'
